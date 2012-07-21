6aa488f94a9d6e4332d2fda31e025c79a055d78b
Steven Haryanto <stevenharyanto@gmail.com>
2012-07-21 03:45:19 +0000

    Import of SHARYANTO/Sah-0.9.0 from CPAN.
    gitpan-cpan-distribution: Sah
    gitpan-cpan-version:      0.9.0
    gitpan-cpan-path:         SHARYANTO/Sah-0.9.0.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Sah.pm
@@ -0,0 +1,31 @@
+package Sah;
+
+our $VERSION = '0.9.0'; # VERSION
+
+1;
+# ABSTRACT: Sah schema (specification)
+
+__END__
+=pod
+
+=head1 NAME
+
+Sah - Sah schema (specification)
+
+=head1 VERSION
+
+version 0.9.0
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
