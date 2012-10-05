d4f799e5038ce21e91e74b06596f8033cc1b57c9
Steven Haryanto <stevenharyanto@gmail.com>
2012-10-05 04:21:34 +0000

    Import of SHARYANTO/Sah-0.9.9 from CPAN.
    gitpan-cpan-distribution: Sah
    gitpan-cpan-version:      0.9.9
    gitpan-cpan-path:         SHARYANTO/Sah-0.9.9.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Sah.pm
+++ lib/Sah.pm
@@ -1,6 +1,6 @@
 package Sah;
 
-our $VERSION = '0.9.7'; # VERSION
+our $VERSION = '0.9.9'; # VERSION
 
 1;
 # ABSTRACT: Schema for data structures (specification)
@@ -14,7 +14,7 @@ Sah - Schema for data structures (specification)
 
 =head1 VERSION
 
-version 0.9.7
+version 0.9.9
 
 =head1 AUTHOR
 
