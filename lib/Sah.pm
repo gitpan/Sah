62f479564b3dae9b5af11040fee0c018a1674c4f
Steven Haryanto <stevenharyanto@gmail.com>
2013-11-20 16:03:49 +0000

    Import of SHARYANTO/Sah-0.9.27 from CPAN.
    gitpan-cpan-distribution: Sah
    gitpan-cpan-version:      0.9.27
    gitpan-cpan-path:         SHARYANTO/Sah-0.9.27.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Sah.pm
+++ lib/Sah.pm
@@ -1,12 +1,15 @@
 package Sah;
 
-our $VERSION = '0.9.26'; # VERSION
+our $VERSION = '0.9.27'; # VERSION
 
 1;
 # ABSTRACT: Schema for data structures (specification)
 
 __END__
+
 =pod
+
+=encoding UTF-8
 
 =head1 NAME
 
@@ -14,7 +17,23 @@ Sah - Schema for data structures (specification)
 
 =head1 VERSION
 
-version 0.9.26
+version 0.9.27
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Sah>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-Sah>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=Sah>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
 
 =head1 AUTHOR
 
@@ -28,4 +47,3 @@ This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
 =cut
-
